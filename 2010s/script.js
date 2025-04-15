c80bef25d7af0b18352b4c39cf3b03507c6d82b1
jtxaudrey <pandaxape@gmail.com>
2025-04-15 03:14:25 -0400

    update

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,11 @@
 function toggleContent() {
 	let content = document.querySelector("#content");
 	content.classList.toggle('hidden');
+	let button = document.querySelector("#mainButton");
+	button.classList.toggle('open');
+}
+
+function toggleMore() {
+	let more = document.querySelector("#moreContent");
+	more.classList.toggle('hidden');
 }
